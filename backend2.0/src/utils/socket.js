e47c91e2799b0918218fbddf26af157c504ae893
Dev Jaiswal <jaiswaldev479@gmail.com>
2025-08-13 18:07:26 +0530

    minor backend2.0 change

--- backend2.0/src/utils/socket.js
+++ backend2.0/src/utils/socket.js
@@ -7,7 +7,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [`${process.env.CORS_ORIGIN}`],
     credentials: true,
     methods: ["GET", "POST"],
   },
